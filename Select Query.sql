de4b86caf3d65c7645340042cc46760f6672d6d8
Md Masipul Islam Siam <masipulisalm@gmail.com>
2023-10-25 01:24:52 +0600

    where query

--- Select Query.sql
+++ Select Query.sql
@@ -6,3 +6,16 @@ from student;
 
 select Name
 from student;
+
+
+select * from student
+where Roll = '0001';
+
+select Name from student
+where Roll = '0001';
+
+select * from course
+where (University, CourseName) = ('BUET','CSE');
+
+select CourseName from course
+where (University, Credit) = ('BUET','130');
