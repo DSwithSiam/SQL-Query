e6a287b58beec81604fd901659d1b22aacbacc13
Md Masipul Islam Siam <masipulisalm@gmail.com>
2023-10-25 03:00:57 +0600

    Comparison Operators

---
+++ Arithmatic Operators.sql
@@ -0,0 +1,20 @@
+select 20 + 10;
+
+select Credit + 20
+from Course;
+
+create table marks (
+Roll char(4) primary key,
+Eng int, 
+Math int
+);
+
+insert into marks (Roll, Eng, Math) values('0001', 80, 96);
+insert into marks (Roll, Eng, Math) values('0002', 60, 78);
+insert into marks (Roll, Eng, Math) values('0003', 80, 98);
+insert into marks (Roll, Eng, Math) values('0004', 85, 88);
+
+delete from marks;
+
+select Eng + Math
+from Marks;
