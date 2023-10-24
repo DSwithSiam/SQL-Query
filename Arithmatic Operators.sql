d412b2730f3ee0d8a52364caef3b96f38a2fa360
Md Masipul Islam Siam <masipulisalm@gmail.com>
2023-10-25 04:03:01 +0600

    in, not in , as

--- Arithmatic Operators.sql
+++ Arithmatic Operators.sql
@@ -1,3 +1,17 @@
+create table Course
+( 
+	University varchar(100),
+    CourseName varchar(50),
+    Credit int not null,
+    
+    primary key(University,CourseName)
+);
+
+insert into course(University, CourseName, Credit) values('BUET', 'CSE', 140);
+insert into course(University, CourseName, Credit) values('BUET', 'EEE', 130);
+insert into course(University, CourseName, Credit) values('DIU', 'EEE', 130);
+insert into course(University, CourseName, Credit) values('DIU', 'CSE', 140);
+
 select 20 + 10;
 
 select Credit + 20
---
+++ in, not in, like, as.sql
@@ -0,0 +1,21 @@
+select *
+from Student
+where roll in(1, 2);
+
+select *
+from Student
+where roll not in(1);
+
+select *
+from Student
+where name like '%sia%';
+
+select *
+from Student
+where name like '%tamim%';
+
+select Name as NM
+from Student;
+
+
+
