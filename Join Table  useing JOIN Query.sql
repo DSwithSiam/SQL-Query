177f2eee60fb44329d1fcdedc4b2a7ba2cdd41b7
Md Masipul Islam Siam <masipulisalm@gmail.com>
2023-11-01 22:29:38 +0600

    left, right, cross join

--- Join Table  useing JOIN Query.sql
+++ Join Table  useing JOIN Query.sql
@@ -1,9 +1,29 @@
 use dummydb;
 
+-- inner join / join
+
 select employees.first_name, departments.department_name
 from employees 
 	join departments on employees.department_id = departments.department_id;
     
 select employees.first_name, departments.department_name
 from employees 
-	join departments using (department_id);
+	join departments using (department_id); -- table same name --
+
+-- left join
+
+select employees.first_name, departments.department_name
+from employees 
+	left join departments on employees.department_id = departments.department_id;
+
+-- Right join
+
+select employees.first_name, departments.department_name
+from employees 
+	right join departments on employees.department_id = departments.department_id;
+
+-- Cross join
+
+select employees.first_name, departments.department_name
+from employees 
+	cross join departments on employees.department_id = departments.department_id;
